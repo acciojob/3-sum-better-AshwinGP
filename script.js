b226f36a72d2ac662d0d38f7c6d723dc2f9e3c11
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-13 20:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	var a=0;
+	var b=0;
+	for(let i=0;i<arr.length;i++){
+		a=a+[i];
+		if(target==a-1||target==a+1){
+			b=a;
+		}
+	}
+  return b;
 }
 
 module.exports = threeSum;
